f145a7dc1d7e8d1e64b91d2642e0893b48ae6341
Rohit Kumar Sah <rohitks.com.np@gmail.com>
2024-05-28 22:04:04 +0545

    Add files via upload

---
+++ eShop/LogIn.php
@@ -0,0 +1,213 @@
+<?php
+
+session_start();
+// session_destroy();
+
+$conn = mysqli_connect("localhost", "root","", "ecommerce");
+// Check if both email and password are provided
+$login_success= "";
+$login_failed="";
+$usercheck="";
+if(isset($_POST['submit'])){
+    $email= $_SESSION['email']= $_POST['email'];
+    $password=$_SESSION['password']=$_POST['password'];
+    
+
+
+if (!empty($email) && !empty($password)) {
+    // Fetch user based on email
+    $selectSql = "SELECT * FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $selectSql);
+    if (mysqli_num_rows($result) > 0) {
+        // User found, check password
+        $row = mysqli_fetch_assoc($result);
+        if ($row['password'] == $password) {
+            $_SESSION['user_id']=$row['id'];
+            $login_success = "Login granted sucessfully !";
+        //    redirect to the home page 
+        $url = 'home.php'; // Replace this with your desired URL
+        $delay = 0; // Delay in seconds
+
+        header("refresh:$delay;url=$url");
+        exit; // To prevent further execution of the script
+        } else {
+        //    echo"Incorrect password";
+            $login_failed= "Incorrect password";
+        }
+    } else{
+        $usercheck="User not found ! please create account";
+    }
+    
+}
+}
+// echo "password is: ".$email,$password;
+mysqli_close($conn); // Close the connection
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignUp</title>
+    <style>
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 95vh;
+        }
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            width: 40%;
+            max-width: 500px;
+            min-width: 320px;
+            min-height: 400px;
+            /* height: 70%; */
+            border-radius: 7px;
+            padding: 10px;
+            background-color: rgb(0, 35, 51);
+            color: white;
+            /* margin: 10px; */
+        }
+        .container h3{
+            text-align: center;
+        }
+a{
+    text-decoration: none;
+}
+        .top_part {
+            /* background-color: black; */
+            height: 60px;
+
+        }
+
+        .bottom_part {
+            /* background-color: red; */
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            position: relative;
+
+        }
+
+        .bottom_part h1 {
+            color: rgb(0, 0, 184);
+            /* background-color: red; */
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            font-weight: 800;
+        }
+
+        form {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            /* align-items: center; */
+            /* background-color: blue; */
+            /* padding-top: 15px; */
+
+        }
+
+
+        .form_fillup {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            column-gap: 10px;
+        }
+
+        .form_fillup input {
+            background-color: transparent;
+           color: white;
+           
+            /* padding-left: 20px; */
+            font-size: 17px;
+            height: 30px;
+            /* border-radius: 7px; */
+            outline: none;
+            border: 0;
+            border-bottom: 2px solid rgb(100, 70, 20);
+            /* border: 1px solid; */
+            width: 70%;
+            min-width: 250px;
+            margin-top: 10px;
+
+        }
+
+        form button {
+            width: 68%;
+            min-width: 270px;
+            border-radius: 7px;
+            border: none;
+            margin-top: 10px;
+            margin-bottom: 10px;
+            /* width: 100%; */
+            height: 35px;
+            cursor: pointer;
+        }
+        form button:hover{
+            background-color: rgb(214, 219, 224);
+        }
+        .terms_condition{
+            text-align: center;
+            padding: 0px 20px 0px 20px;
+            opacity: 80%;
+            /* background-color: red; */
+        }
+        p{
+            text-align: center;
+            margin-top: -10px;
+            /* color: red; */
+        }
+        a{
+            /* color: rgb(250, 189, 5); */
+        }
+    </style>
+</head>
+<script>
+
+    // After 5 seconds, hide the message
+    setTimeout(function(){
+        document.getElementById("login_failed_message").style.display = "none";
+        document.getElementById("usercheck").style.display = "none";
+        
+
+    }, 5000); // 5000 milliseconds = 5 seconds
+</script>
+<body>
+    
+
+
+
+    <div class="container">
+        <h3 style="color: red;"><?php echo $login_success;
+         echo "<span id='login_failed_message'>$login_failed</span>"; // Display the message
+         echo "<span id='usercheck'>$usercheck</span>"; // Display the message
+          ?></h3>
+        <div class="top_part"><a href="index.php"><img src="images/e_shop.png" width="160px" alt=""></a></div>
+        <div class="bottom_part">
+            <h1>Log In<Inp></Inp></h1>
+            <form  method="post" action="LogIn.php">
+                <div class="form_fillup">
+
+                    <input type="email" name="email" placeholder="Email" required  >
+                    <input type="password" name="password" placeholder="Password" required>
+                </div>
+                <button name="submit">Submit</button>
+            </form>
+        </div>
+        <div class="No_account">
+            <h3>Don't have an account? <a href="SignUp.php">Sign Up</a></h3>
+            <p>Reset your password ? <a href="resetpassword.php">Click here</a></hp>
+        </div>
+        </div>
+</body>
+
+</html>
---
+++ eShop/index.php
@@ -0,0 +1,181 @@
+<?php
+session_start();
+$conn = mysqli_connect("localhost", "root", "", "ecommerce");
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM products";
+$result1 = mysqli_query($conn, $sql);
+
+if (isset($_POST['action'])) {
+    if ($_POST['action'] == 'add_to_cart') {
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $price = $_POST['price'];
+        $quantity = $_POST['quantity'];
+        $image = $_POST['image'];
+        if (!isset($_SESSION['cart'][$id])) {
+            $_SESSION['cart'][$id] = array(
+                'id' => $id,
+                'name' => $name,
+                'price' => $price,
+                'quantity' => $quantity,
+                'image' => $image
+            );
+        }
+    } elseif ($_POST['action'] == 'buy_now') {
+        header("Location: mycart.php");
+        exit;
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="splash.css">
+    
+<style>
+
+    #navbar{
+        position: fixed;
+        
+        /* background-color: red; */
+        /* color: ; */
+    }
+    .content_home{
+        padding-top: 100px;
+    }
+</style>
+</head>
+
+<body>
+    <!-- splash screen code here -->
+    <div id="splash-screen">
+        <div class="splash-content">
+            <img src="e_shop.png" alt="images/e-Shop Logo" width="300px" class="splash-image">
+            <h1>Welcome to <span> e-Shop</span></h1>
+            <div class="loader"></div>
+        </div>
+    </div>
+    <!-- nav bar start from here ************************ -->
+    <nav id="navbar">
+        <div class="left_nav"><img src="e_shop.png" width="100%" alt=""></div>
+        <div class="middle_nav">
+            <ul>
+                <li><a href="#home">Home </a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#products">Product</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="myorder.php">Order</a></li>
+            </ul>
+            <div class="bar_icon">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+
+        </div>
+        <div class="right_nav">
+
+            <i class="fa-solid fa-magnifying-glass" onclick="SearchOpenClose();"></i>
+            <div class="cart">
+                <a href="mycart.php"> <i class="fa-solid fa-cart-shopping"></i>
+                    <div class="item_no"><?php echo count($_SESSION['cart'] ?? []); ?></div>
+                </a>
+            </div>
+            <div class="Home_icon">
+                <a href="SignUp.php"><i class="fa-solid fa-house-user"></i></a>
+
+            </div>
+
+        </div>
+        <form class="searchbox" onsubmit="event.preventDefault(); filterItems();">
+            <input id="searchInput" type="search" placeholder="⚡I'm looking for...">
+            <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+        </form>
+    </nav>
+
+    </section>
+    
+    <!-- home pages start -->
+    <section class="content_home" id="home">
+        <div class="left_home">
+            <h1>Welcome to e-Shop</h1>
+            <p>Your Ultimate Online Destination for Everything You Need! Discover a vast array of products, from trendy fashion to cutting-edge gadgets, all at your fingertips.!</p>
+            <button style="background-color: blue;"> <a href="#content-items">Buy Now</a></button>
+        </div>
+        <div class="right_home">
+            <img src="images/home_page_.png" alt="">
+        </div>
+    </section>
+    <!-- home pages end here -->
+    <h1 style="text-align: center;" id="products">Products</h1>
+    <section class="content-items" id="content-items">
+        <div class="items_container">
+            <?php
+            if (mysqli_num_rows($result1) > 0) {
+                while ($row = mysqli_fetch_assoc($result1)) {
+            ?>
+                    <div class="div1">
+                        <div class="product_img">
+                            <img src="<?php echo $row['image']; ?>" alt="">
+                        </div>
+
+                        <form action="" method="post"  >
+                            <h1><?php echo $row['name']; ?></h1>
+                            <div class="upper_form_part">
+                                <label for="name">Rs.<?php echo $row['price']; ?> only</label>
+                                <input type="number" name="quantity" value="1">
+                            </div>
+                            <div class="lower_part_form">
+                                <?php if (isset($_SESSION['cart'][$row['id']])) { ?>
+                                    <input type="submit" name="action" value="Buy Now">
+                                    <input type="hidden" name="action" value="buy_now">
+                                <?php } else { ?>
+                                    <input type="submit" name="action" value="Add to Cart">
+                                    <input type="hidden" name="action" value="add_to_cart">
+                                <?php } ?>
+                                <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                                <input type="hidden" name="name" value="<?php echo $row['name']; ?>">
+                                <input type="hidden" name="price" value="<?php echo $row['price']; ?>">
+                                <input type="hidden" name="image" value="<?php echo $row['image']; ?>">
+                            </div>
+                        </form>
+                    </div>
+            <?php
+                }
+            }
+            ?>
+        </div>
+    </section>
+    
+    <?php include 'CApage/CApage.php' ?>
+    <section id="about">  </section>
+
+    <!-- home page end -->
+    <script src="script.js"></script>
+    <script>
+        // splash screen code here **********
+        setTimeout(function() {
+            const splashScreen = document.getElementById('splash-screen');
+            splashScreen.style.opacity = '0';
+            setTimeout(function() {
+                splashScreen.style.display = 'none';
+            }, 300); // Wait for the fade-out transition
+        }, 3000);
+        // Display the splash screen for 3 seconds
+
+            
+    </script>
+</body>
+
+</html>
+<?php
+
+?>
