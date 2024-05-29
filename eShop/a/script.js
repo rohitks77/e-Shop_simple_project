78edf48c3195b2af747ebf4a158d82cf918b8487
Rohit Kumar Sah <rohitks.com.np@gmail.com>
2024-05-30 00:12:48 +0545

    Delete eShop/a directory

--- eShop/a/script.js
+++
@@ -1,7 +0,0 @@
-function showSection(sectionId) {
-    var sections = document.querySelectorAll('.section');
-    sections.forEach(function(section) {
-        section.classList.remove('active');
-    });
-    document.getElementById(sectionId).classList.add('active');
-}
